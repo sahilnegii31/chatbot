7343a28d8d4e375112a507252bc43d3b0718d99c
sahil <sahilnegii7618@gmail.com>
2026-07-21 22:52:47 +0530

    Chatbot changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,53 +10,53 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 
 const App = () => {
-    const [message, setmessage] = useState([])
-    const [loading, setloading] = useState(false)
+  //   const [message, setmessage] = useState([])
+  //   const [loading, setloading] = useState(false)
     
-    const sendMessage= async (userText)=>{
-      const newmessage = [...message , {role:"user", text: userText}]
-      setmessage(newmessage)
-      setloading(true)
+  //   const sendMessage= async (userText)=>{
+  //     const newmessage = [...message , {role:"user", text: userText}]
+  //     setmessage(newmessage)
+  //     setloading(true)
     
-    try {
-      const response = await axios.post(
-      "https://api.groq.com/openai/v1/chat/completions",
-      {
-        model: "llama-3.3-70b-versatile",
-        messages: newmessage.map(msg => ({
-          role: msg.role === "model" ? "assistant" : msg.role,
-          content: msg.text
-        }))
-      },
-      {
-        headers: {
-          "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`,
-          "Content-Type": "application/json"
-        }
-      }
-    )
-      const reply= response.data.choices[0].message.content
-      setmessage([...newmessage, {role:"model",text : reply}])
-    } catch (error) {
-      console.log("error generating response!!",error)
-    } finally{setloading(false)}
-  }
+  //   try {
+  //     const response = await axios.post(
+  //     "https://api.groq.com/openai/v1/chat/completions",
+  //     {
+  //       model: "llama-3.3-70b-versatile",
+  //       messages: newmessage.map(msg => ({
+  //         role: msg.role === "model" ? "assistant" : msg.role,
+  //         content: msg.text
+  //       }))
+  //     },
+  //     {
+  //       headers: {
+  //         "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`,
+  //         "Content-Type": "application/json"
+  //       }
+  //     }
+  //   )
+  //     const reply= response.data.choices[0].message.content
+  //     setmessage([...newmessage, {role:"model",text : reply}])
+  //   } catch (error) {
+  //     console.log("error generating response!!",error)
+  //   } finally{setloading(false)}
+  // }
 
 
-  const weather= async (userText)=>{
-    try {
-      const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${userText}&limit=5&appid=${VITE_WEATHER_API_KEY}`)
-      console.log(response);
+  // const weather= async (userText)=>{
+  //   try {
+  //     const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${userText}&limit=5&appid=${VITE_WEATHER_API_KEY}`)
+  //     console.log(response);
       
-    } catch (error) {
-      console.log("error retreving data from api ",error);
-    }
+  //   } catch (error) {
+  //     console.log("error retreving data from api ",error);
+  //   }
 
-  }
+  // }
 
 
 
-  const [active, setactive] = useState("Chat")  // for buttons
+  // const [active, setactive] = useState("Chat")  // for buttons
     
     return (
     <div className='bg-[#0f0f0f] h-full px-5 text-white'>
@@ -70,8 +70,9 @@ const App = () => {
           </motion.div>
       })}
       <Button activeMode={active} onModeChange={setactive}/>
-      <ChatInput onSend={sendMessage} loading={loading}/> */}
-      <Home />
+      <ChatInput onSend={sendMessage} loading={loading}/>
+      <Home /> */}
+      <ChatBot />
     </div>
   )
 }
